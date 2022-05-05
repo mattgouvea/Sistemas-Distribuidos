76905869be3bc62d636f46efa1bebe7991bf540f
Matheus <mattgouvea@gmail.com>
2022-05-05 01:34:37 -0300

    lab3

--- lab3/cliente.py
+++ lab3/cliente.py
@@ -1,6 +1,6 @@
 import socket
 
-HOST = 'localhost' # maquina onde esta o par passivo
+HOST = 'localhost'  # maquina onde esta o par passivo
 PORTA = 5000        # porta que o par passivo esta escutando
 
 # cria socket
@@ -9,14 +9,14 @@ sock = socket.socket() # default: socket.AF_INET, socket.SOCK_STREAM
 # conecta-se com o par passivo
 sock.connect((HOST, PORTA)) 
 
-# envia uma mensagem para o par conectado, que sera o nome do arquivo
-sock.send(b"texto.txt")
-
-#espera a resposta do par conectado (chamada pode ser BLOQUEANTE)
-msg = sock.recv(1024) # argumento indica a qtde maxima de bytes da mensagem
-
-# imprime a mensagem recebida
-print(bytes.decode(msg))
+# envia uma mensagem para o par conectado
+# a mensagem "q" encerra a troca de mensagens e termina o programa
+while True:
+    msg = input("Escreva a mensagem: ")
+    if str(msg) == "q": break
+    sock.send(bytes(msg, 'utf-8'))
+    msg = sock.recv(1024)
+    print(str(msg,  encoding='utf-8'))
 
 # encerra a conexao
-sock.close() 
+sock.close()
